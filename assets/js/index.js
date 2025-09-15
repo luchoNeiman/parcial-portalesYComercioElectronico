cd79cacd3ea8d9558993729f00e647444d4925c7
luchoNeiman <neimanlucho@gmail.com>
2025-09-14 23:30:54 -0300

    dashboard corregido

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const finalizarBtn = document.querySelector('.btn-primario.w-100');
+    const finalizarBtn = document.querySelector('#finalizar-compra');
     if (finalizarBtn) {
         finalizarBtn.addEventListener('click', function (e) {
             e.preventDefault();
             const modal = new bootstrap.Modal(document.getElementById('modalCompraExitosa'));
+            modal.show();
+        });
+    }
+
+
+    const submitFormContact = document.querySelector('#form-contacto');
+    if (submitFormContact) {
+        submitFormContact.addEventListener('submit', function (e) {
+            e.preventDefault();
+            const modal = new bootstrap.Modal(document.getElementById('modalformContacto'));
             modal.show();
         });
     }
